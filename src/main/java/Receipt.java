49f3ae261f8b61e5aa6d8c4bdf126b78b0fa294d
Mateusz Roszkowski <229990@edu.p.lodz.pl>
2020-12-05 17:21:57 +0100

    Dodanie rzeczy do Receipt

--- src/main/java/Receipt.java
+++ src/main/java/Receipt.java
@@ -1,12 +1,24 @@
 import java.util.List;
 
 public class Receipt {
-    private String TotalPrice;
-    private String ShopName;
-    private List<String> tag;
-    private String scan;
+    private double TotalPrice;
+    private final String ShopName;
+    private final List<String> tags;
+    private final List<Purchase> purchases;
+    private final String scan;
 
-    public String getTotalPrice() {
+    public Receipt(String shopName, List<String> tags, List<Purchase> purchases, String scan) {
+        if (shopName.length() == 0) {
+            throw new IllegalArgumentException("Invalid shop name!");
+        }
+        ShopName = shopName;
+        this.tags = tags;
+        this.purchases = purchases;
+        //dodac wyjątek sprawdzający istnienie pliku
+        this.scan = scan;
+    }
+
+    public double getTotalPrice() {
         return TotalPrice;
     }
 
@@ -14,8 +26,12 @@ public class Receipt {
         return ShopName;
     }
 
-    public List<String> getTag() {
-        return tag;
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public List<Purchase> getPurchases() {
+        return purchases;
     }
 
     public String getScan() {
