fafbc384092f48ae1d713eec1770ddac0fa2f104
Mateusz Roszkowski <229990@edu.p.lodz.pl>
2020-12-05 17:00:00 +0100

    Dodanie klasy purchase

--- src/main/java/Purchase.java
+++ src/main/java/Purchase.java
@@ -1,2 +1,19 @@
 public class Purchase {
+    private final String name;
+    private final double price;
+
+    public Purchase(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+
 }
