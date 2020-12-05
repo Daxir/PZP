b4fb55ef8f71044d4ecceff17c46012d40a68dd5
229872 <bartek.dygasinski00@gmail.com>
2020-12-05 17:12:22 +0100

    Receipt

--- src/main/java/Receipt.java
+++ src/main/java/Receipt.java
@@ -1,2 +1,24 @@
+import java.util.List;
+
 public class Receipt {
+    private String TotalPrice;
+    private String ShopName;
+    private List<String> tag;
+    private String scan;
+
+    public String getTotalPrice() {
+        return TotalPrice;
+    }
+
+    public String getShopName() {
+        return ShopName;
+    }
+
+    public List<String> getTag() {
+        return tag;
+    }
+
+    public String getScan() {
+        return scan;
+    }
 }
