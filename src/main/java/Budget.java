c65a0cb58f7969c5735b56b1e630868fbcc630c6
229871 Dyczka Monika <229871@edu.p.lodz.pl>
2020-12-05 17:22:33 +0100

    budget

--- src/main/java/Budget.java
+++ src/main/java/Budget.java
@@ -1,2 +1,26 @@
 public class Budget {
+    private double currentSpendings;
+    private double monthlyBudget;
+    private double remainingSpendings;
+    private ReceiptRepository receiptRep;
+
+    public Budget(double monthlyBudget, ReceiptRepository receiptRep) {
+        this.monthlyBudget = monthlyBudget;
+        this.receiptRep = receiptRep;
+        this.remainingSpendings = monthlyBudget;
+        this.currentSpendings = 0.0;
+    }
+
+    public double getCurrentSpendings() {
+        return currentSpendings;
+    }
+
+    public double getMonthlyBudget() {
+        return monthlyBudget;
+    }
+
+    public double getRemainingSpendings() {
+        return remainingSpendings;
+    }
+
 }
