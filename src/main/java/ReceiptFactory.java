a75d0e125e13385acda05c3ad2e88f3784ca18c0
mmalarski <malarsky.mike@gmail.com>
2020-12-05 17:26:35 +0100

    recepiptrepo i fabryka

--- src/main/java/ReceiptFactory.java
+++ src/main/java/ReceiptFactory.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReceiptFactory {
+    public Receipt createReceipt(String shopName, String tag, String pathToScan, List<Purchase> purchases) {
+        List<Purchase> purchToInput = new ArrayList<Purchase>();
+
+
+
+        ReceiptRepository.add(newr);
+        return newr;
+    }
 }
--- src/main/java/ReceiptRepository.java
+++ src/main/java/ReceiptRepository.java
@@ -1,2 +1,14 @@
+import java.util.List;
+
 public class ReceiptRepository {
+    private List<Receipt> receipts;
+
+    public List<Receipt> getRepository() {
+        return receipts;
+    }
+
+    public boolean add(Receipt r){
+        receipts.add(r);
+        return receipts.add(r);
+    }
 }
