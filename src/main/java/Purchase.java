a7874e7b7f2ab487f20335919bed3c5a90a88da5
Mateusz Roszkowski <229990@edu.p.lodz.pl>
2020-12-05 17:05:56 +0100

    Implementacja klasy Purchase

--- src/main/java/Purchase.java
+++ src/main/java/Purchase.java
@@ -1,10 +1,17 @@
-public class Purchase {
+import java.io.Serializable;
+
+public class Purchase implements Serializable {
     private final String name;
     private final double price;
+    private final int quantity;
 
-    public Purchase(String name, double price) {
+    public Purchase(String name, double price, int quantity) {
+        if (price <= 0) {
+            throw new NumberFormatException("Invalid price!");
+        }
         this.name = name;
         this.price = price;
+        this.quantity = quantity;
     }
 
     public String getName() {
@@ -15,5 +22,11 @@ public class Purchase {
         return price;
     }
 
+    public int getQuantity() {
+        return quantity;
+    }
 
+    public double getTotalPurchaseValue() {
+        return getPrice() * getQuantity();
+    }
 }
