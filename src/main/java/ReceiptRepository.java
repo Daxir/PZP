0a193bfe36e29064ff1c6fa77fa572b55cb08a7b
Mateusz Roszkowski <229990@edu.p.lodz.pl>
2020-12-05 17:37:35 +0100

    Dodanie Subject do ReceiptRepository

--- src/main/java/ReceiptRepository.java
+++ src/main/java/ReceiptRepository.java
@@ -1,7 +1,11 @@
+import jdk.internal.loader.AbstractClassLoaderValue;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class ReceiptRepository {
+public class ReceiptRepository implements Subject {
     private List<Receipt> receipts;
+    private final List<Observer> observers = new ArrayList<Observer>();
 
     public List<Receipt> getRepository() {
         return receipts;
@@ -18,5 +22,17 @@ public class ReceiptRepository {
             spendings += r.getTotalPrice();
         }
         return spendings;
+    }
+
+    public void attach(Observer o) {
+        observers.add(o);
+    }
+
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+
+    public void notifyObservers() {
+        observers.forEach(Observer::update);
     }
 }
