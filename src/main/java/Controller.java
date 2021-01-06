a0ae7d50de5be36243d6ef13806d719cd759bd49
229871 Dyczka Monika <229871@edu.p.lodz.pl>
2021-01-06 17:41:52 +0100

    dodanie pol do controllera

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,11 +2,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -23,6 +20,21 @@ public class Controller {
     public Button loginButton;
     private ReceiptRepository receiptRepository = new ReceiptRepository();
     public ImageView imageView;
+    public TextField shopName;
+    public Button chooseScanButton;
+    public TextField scanTextField;
+    public DatePicker dateOfPurchaseDatePicker;
+    public TextArea tagsTextArea;
+    public ListView<Receipt> purchasesListView;
+    public Button addToPurchasesButton;
+    public TextField productNameTextField;
+    public TextField priceTextField;
+    public TextField quantityTextField;
+    public Button addReceiptButton;
+    public Button doneButton;
+
+
+
 
     public void zniknijbaton() {
         System.out.println("welcome");
